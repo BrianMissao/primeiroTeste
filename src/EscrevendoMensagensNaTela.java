e4bbead612869bfe48c53354d32a2901d4614bf3
Brian Missao <brianmissao@dgtbr.com.br>
2020-06-19 18:12:10 -0400

    #Corrigindo nome de classe

--- src/EscrevendoMensagensNaTela.java
+++ src/EscrevendoMensagensNaTela.java
@@ -1,4 +1,4 @@
-public class Principal {
+public class EscrevendoMensagensNaTela {
     public static void main(String[] args) {
         System.out.println("Este é o meu primeiro teste!\nSeria este um programa?\nNão consigo fazer nada além de mostrar conteúdo fixo?\nComo o exemplo original era em html, o texto de um link será impresso abaixo:\nConheça o site da nossa editora: http://www.casadocodigo.com.br");
 //Como não estou trabalhando com JavaScript, vou modificar a mensagem para o contexto atual.
