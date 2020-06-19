12605ce298a2704ec2703442d41c47c3671ce902
Brian Missao <brianmissao@dgtbr.com.br>
2020-06-19 17:24:39 -0400

    #Primeiro Commit

---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Este é o meu primeiro teste!\nSeria este um programa?\nNão consigo fazer nada além de mostrar conteúdo fixo?\nComo o exemplo original era em html, o texto de um link será impresso abaixo:\nConheça o site da nossa editora: http://www.casadocodigo.com.br");
+//Como não estou trabalhando com JavaScript, vou modificar a mensagem para o contexto atual.
+        System.out.println("Podemos fazer mais com Java!");
+    }
+}
